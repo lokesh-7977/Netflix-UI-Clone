187ff1ddbca27d73f060b468fdeb2aa72d564968
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2025-01-06 00:39:08 +0530

    refactor:fix middleware

--- middleware.ts
+++ middleware.ts
@@ -4,32 +4,28 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 const authMiddleware = withAuth({
   callbacks: {
     authorized: ({ token }) => {
-      // Authorize only if a token exists
       return !!token;
     },
   },
   pages: {
-    signIn: "/auth/sign-in", // Redirect to sign-in page if unauthorized
+    signIn: "/auth/sign-in",
   },
 });
 
 export default function middleware(req: NextRequest, event: NextFetchEvent) {
   const url = req.nextUrl.clone();
-  const token = req.cookies.get("next-auth.session-token") || req.cookies.get("__Secure-next-auth.session-token");
+  const token =
+    req.cookies.get("next-auth.session-token") ||
+    req.cookies.get("__Secure-next-auth.session-token");
 
-  // If a token exists and the user tries to access restricted routes, redirect to /browse
-  if (
-    token &&
-    ["/", "/auth/sign-in", "/auth/sign-up"].includes(url.pathname)
-  ) {
+  if (token && ["/", "/auth/sign-in", "/auth/sign-up"].includes(url.pathname)) {
     url.pathname = "/browse";
     return NextResponse.redirect(url);
   }
 
-  // Pass the request to NextAuth's withAuth middleware
   return authMiddleware(req as NextRequestWithAuth, event);
 }
 
 export const config = {
-  matcher: ["/browse", "/", "/auth/:path*"], // Add all protected and auth-related routes
+  matcher: ["/browse"],
 };
